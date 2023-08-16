911f81fe7e9a5fb336445bb496fea43622d3743a
이현웅 <lhw0507@midasit.com>
2023-08-16 14:49:55 +0900

    daum 블로그 검색 api 서버

---
+++ src/main/kotlin/com/example/study/blog/service/BlogService.kt
@@ -0,0 +1,55 @@
+package com.example.study.blog.service
+
+import com.example.study.blog.dto.BlogDto
+import com.example.study.blog.entity.Wordcount
+import com.example.study.blog.repository.WordRepository
+import com.example.study.core.exception.InvalidInputException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+
+@Service
+class BlogService (
+    val wordRepository: WordRepository
+){
+    @Value("\${REST_API_KEY}")
+    lateinit var restAPIKey: String
+
+    fun searchKakao(blogDto: BlogDto): String? {
+        val webClient = WebClient
+            .builder()
+            .baseUrl("https://dapi.kakao.com")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build()
+
+        val response = webClient
+            .get()
+            .uri {
+                it.path("/v2/search/blog")
+                    .queryParam("query", blogDto.query)
+                    .queryParam("sort", blogDto.sort)
+                    .queryParam("page", blogDto.page)
+                    .queryParam("size", blogDto.size)
+                    .build()
+            }
+            .header("Authorization", "KakaoAK $restAPIKey")
+            .retrieve()
+            .bodyToMono<String>()
+
+        val result = response.block()
+
+        val lowQuery: String = blogDto.query.lowercase()
+        val word: Wordcount = wordRepository.findById(lowQuery).orElse(Wordcount(lowQuery))
+        word.cnt++
+
+        wordRepository.save(word)
+
+        return result
+    }
+
+    fun searchWordRank(): List<Wordcount> = wordRepository.findTop10ByOrderByCntDesc()
+
+}
